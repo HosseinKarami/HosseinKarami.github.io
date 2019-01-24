88ab456cf3eab663dd06cc696df15272a5afca24
Hossein Karami <hi@hosseinkarami.com>
2019-01-24 19:13:13 +0330

    Fixed: Accessibility

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -8,26 +8,4 @@
  * Copyright 2019. MIT licensed.       *
  +-------------------------------------+
  */
-(function ($, window, document, undefined) {
 
-  'use strict';
-
-  $(function () {
-    var obj = $('html');
-
-    $('.js .menu-open').on('click', function(){
-
-      if (obj.hasClass('js-menu-open'))
-      {
-        obj.removeClass('js-menu-open');
-      }
-      else
-      {
-        obj.addClass('js-menu-open');
-      }
-
-      return false;
-    });
-  });
-
-})(jQuery, window, document);
